0d34ce9415d3af6e85d9dd6ab54409c56e531e38
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-03-07 03:58:50 -0300

    Agregué la funcionalidad del consumo continuo de la API para mostrar nuevo contenido en la lista de peliculas

--- app/src/main/java/com/example/challengeandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/challengeandroidkotlin/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.challengeandroidkotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.challengeandroidkotlin.Adapter.MovieAdapter
 import com.example.challengeandroidkotlin.Adapter.MovieAdapter.OnMovieClickListener
 import com.example.challengeandroidkotlin.databinding.ActivityMainBinding
@@ -20,6 +22,9 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MovieAdapter
     private val movieImages = mutableListOf<ResultResponse>()
+    lateinit var layoutManager: LinearLayoutManager
+
+    private var page = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,27 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
         setContentView(binding.root)
         initRecyclerView()
         listPopularMovies()
+
+        layoutManager= LinearLayoutManager(this)
+        binding.rvMovie.layoutManager=layoutManager
+
+        //API is called when the scroll reaches the end, to display new content
+        binding.rvMovie.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0){
+                    val visibleItemCount= layoutManager.childCount
+                    val pastVisibleItem= layoutManager.findFirstCompletelyVisibleItemPosition()
+                    val total= adapter.itemCount
+
+                    println("$visibleItemCount - $pastVisibleItem - $total")
+                    if ((visibleItemCount + pastVisibleItem) >= total) {
+                        page++
+                        listPopularMovies()
+                    }
+                }
+                super.onScrolled(recyclerView, dx, dy)
+            }
+        })
     }
 
     private fun initRecyclerView(){
@@ -46,14 +72,19 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
     //Call the api, save the response and display it in RecyclerView
     private fun listPopularMovies(){
         CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit().create(APIService::class.java).getMoviePopular("popular?api_key=a9548caf23fe23d2ebb393ca325cd3bf&language=en-US&page=1")
+            val call = getRetrofit().create(APIService::class.java).getMoviePopular("popular?api_key=a9548caf23fe23d2ebb393ca325cd3bf&language=en-US&page=$page")
             val moviesData = call.body()?.results
 
             runOnUiThread {
                 if(call.isSuccessful){
-                    movieImages.clear()
                     movieImages.addAll(moviesData!!)
-                    adapter.notifyDataSetChanged()
+
+                    if (::adapter.isInitialized) {
+                        adapter.notifyDataSetChanged()
+                    } else {
+                        adapter = MovieAdapter(movieImages,this@MainActivity)
+                        binding.rvMovie.adapter = adapter
+                    }
 
                 }else{
                     showError()
@@ -73,5 +104,6 @@ class MainActivity : AppCompatActivity(), OnMovieClickListener {
         intent.putExtra("id","$id")
         startActivity(intent)
     }
+
 }
 
